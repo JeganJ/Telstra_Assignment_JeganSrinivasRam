61e4ff7952503a9241691fa9fbb1de5dc4e7dac7
JeganJ <jeganramji@gmail.com>
2015-04-08 15:18:22 +0530

    Added extra comments for Fix the Bugs Projects.

--- ProficiencyTestSolution/Question1FixTheBugs/Program.cs
+++ ProficiencyTestSolution/Question1FixTheBugs/Program.cs
@@ -7,9 +7,11 @@ namespace Question1FixTheBugs
 {
     #region Question 1
     /*
-     * Logic wise, there should be braces around a and b
-     * Result wise, it will always return integer and not double
-     *
+     * Bug 1 : Logic wise, there should be braces around a and b 
+     *      Fix for Bug 1 : added braces around a + b
+     * 
+     * Bug 2 : Result wise, it will always return integer and not double 
+     *      Fix for Bug 2 : converted ( a + b ) as double
      */
     public class MathUtils
     {
